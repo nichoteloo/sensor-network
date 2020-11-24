c74e33d6bbbc4c25b96fdcab17285628b508a525
nichoteloo <nicolaschristianto@mail.ugm.ac.id>
2020-11-24 12:24:30 +0700

    Sensor Network

---
+++ XBee/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!E:\PROJECT\VSCode Python Project\Sensor Network\Django\XBee\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
